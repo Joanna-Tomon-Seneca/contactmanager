e4b948ef81e745cec8c52248aa4d97b8f975610a
Joanna Tomon <joanna.tomon@senecacollege.ca>
2018-12-20 15:55:15 -0500

    Updates

---
+++ precache-manifest.79a482255cee1e6d881e872c3c08b791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "114ece4c0fe08382250c",
+    "url": "/contactmanager/static/js/main.114ece4c.chunk.js"
+  },
+  {
+    "revision": "7b5c60d3f26811fb56db",
+    "url": "/contactmanager/static/js/1.7b5c60d3.chunk.js"
+  },
+  {
+    "revision": "114ece4c0fe08382250c",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "7b5c60d3f26811fb56db",
+    "url": "/contactmanager/static/css/1.68d4ffe0.chunk.css"
+  },
+  {
+    "revision": "3010425328b1d4cf322a49dda81ac754",
+    "url": "/contactmanager/index.html"
+  }
+];
